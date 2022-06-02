5860325de6f7f58923f34128811a366154805413
Eduardo Olivo <102386598+eolivo01@users.noreply.github.com>
2022-06-01 22:05:31 -0400

    varios cambios

--- api/app.js
+++ api/app.js
@@ -1,31 +1,41 @@
-const express = require('express');
-const path = require('path');
-const https = require('https');
-const querystring = require("querystring")
+const express = require('express'); // framework para levantar API o microservicos con javascript
+const path = require('path'); // Acceder al sistema opeartivo para saber en que carpeta estamos
+const https = require('https'); // Hacer llamadas a servivios
+// const querystring = require("querystring") 
 
 const app = express();
 const root = path.resolve(__dirname, '..');
 
 // Log invocations
-app.use(function (req, res, next) { console.log(req.url); next(); });
+app.use(function (req, res, next) { console.log(req.url); next(); }); 
 
 // Directly serve static content from /client
-app.use(express.static(root + '/client'));
+app.use(express.static(root + '/client')); 
 
-// Simple REST API that returns some entities
+// Simple REST API that returns some entities (API's con verbos)
 app.get('/api/entities', (req, res) =>
   res.send({
     entities:
       ['Q2887',
-        'Q33986'
+        'Q33986',
+        'Eduardo',
+        'Roberto',
+        'Pablo'
       ]
   })
 );
 
-app.get('/api/entities/:id', (req, res) => {
-  console.log(`Searching ${req.params.id}`);
+app.get('/api/deportes', (req, res) => {
+  //console.log(`Searching ${req.params.id}`);
   const queryParams = new URLSearchParams(
-    [['query', `select * where { wd:Q${req.params.id} rdfs:label $label . FILTER (lang($label) = 'es')}`],
+    [['query', `SELECT DISTINCT ?deporte ?deporteLabel ?deporteDescription ?deporteImage
+    WHERE
+    {
+    ?deporte wdt:P31 wd:Q31629.
+    ?deporte wdt:P18 ?deporteImage
+    SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+    }
+    ORDER BY ASC (?deporteLabel)`],
     ['format', 'json']
     ]).toString();
   const options = {
@@ -47,11 +57,8 @@ app.get('/api/entities/:id', (req, res) => {
       console.log(`Result obtained:\n${result}\n---`);
       const json = JSON.parse(result);
       const bindings = json.results.bindings;
-      const label = bindings.length > 0 ? bindings[0].label.value : 'Not found';
-      res.send({
-        entity: `${req.params.id}`,
-        label: `${label}`
-      })
+     // const label = bindings.length > 0 ? bindings[0].label.value : 'Not found';
+      res.send(json)
     });
   }).on('error', err => {
     console.log('Error: ', err.message);
--- client/code.js
+++ client/code.js
@@ -1,18 +1,39 @@
 async function getEntities() {
-    const response = await fetch('/api/entities');
+    const response = await fetch('/api/deportes');
     const data = await response.json();
     return data
 }
 
 function fillEntities() {
+    localStorage.setItem('item','hola')
     getEntities().then(data => {
-        console.log(data.entities);
+        console.log(data.results.bindings);
         const ulEntities = document.getElementById("entities");
-        data.entities.forEach(entity => {
-          const liEntity = document.createElement("li");
-          const text = document.createTextNode(entity);
-          liEntity.appendChild(text);
+        //const item = data.results.bindings [0];
+        //localStorage.setItem(item.deporteLabel.value, JSON.stringify(item))
+        data.results.bindings.forEach(entity => {
+          const liEntity = document.createElement('li');
+          const imageEntity = document.createElement("img");
+          const deporteLabel = document.createTextNode(entity.deporteLabel.value);
+          liEntity.appendChild(deporteLabel);
+          imageEntity.setAttribute("src",entity.deporteImage.value);
+          imageEntity.setAttribute("width",'50px');
+          imageEntity.setAttribute("height",'50px');
+          liEntity.appendChild(imageEntity);
           ulEntities.appendChild(liEntity);
         })
     })
+}
+
+function mostrar() {
+    const item = localStorage.getItem('item');
+    console.log("item",item)
+}
+
+function seter() {
+    localStorage.setItem('item','hola')
+}
+
+function borrar(){
+localStorage.removeItem('item')
 }
