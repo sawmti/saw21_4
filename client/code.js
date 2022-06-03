2950a6ec2229622906db9691419b52d518f175f7
Eduardo Olivo <102386598+eolivo01@users.noreply.github.com>
2022-06-02 21:29:04 -0400

    Cambios en el front

--- client/code.js
+++ client/code.js
@@ -60,38 +60,3 @@ function seter() {
 function borrar(){
 localStorage.removeItem('item')
 }
-
-function genera_tabla() {
-    // Obtener la referencia del elemento body
-    var body = document.getElementsByTagName("body")[0];
-  
-    // Crea un elemento <table> y un elemento <tbody>
-    var tabla   = document.createElement("table");
-    var tblBody = document.createElement("tbody");
-  
-    // Crea las filas
-    for (var i = 0; i < 3; i++) {
-      // Crea las columnas de la tabla
-      var hilera = document.createElement("tr");
-  
-      for (var j = 0; j < 3; j++) {
-        // Crea un elemento <td> y un nodo de texto, haz que el nodo de
-        // texto sea el contenido de <td>, ubica el elemento <td> al final
-        // de la hilera de la tabla
-        var celda = document.createElement("td");
-        var textoCelda = document.createTextNode("celda en la hilera "+i+", columna "+j);
-        celda.appendChild(textoCelda);
-        hilera.appendChild(celda);
-      }
-  
-      // agrega la hilera al final de la tabla (al final del elemento tblbody)
-      tblBody.appendChild(hilera);
-    }
-  
-    // posiciona el <tbody> debajo del elemento <table>
-    tabla.appendChild(tblBody);
-    // appends <table> into <body>
-    body.appendChild(tabla);
-    // modifica el atributo "border" de la tabla y lo fija a "2";
-    tabla.setAttribute("border", "2");
-  }
